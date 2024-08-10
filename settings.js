db8a182f9cb39f56fbe98434a9d86b8f65ac8a4b
Jonathannajjzd <166914724+Jonathannajjzd@users.noreply.github.com>
2024-08-10 23:27:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.location = "Nigeria, Lagos" //ur location
 
 //new
 global.botname = 'QUEEN_ANITA-V3' //ur bot name
-global.ownernumber = ' ' //ur owner number
+global.ownernumber = '25765809718' //ur owner number
 global.ownername = "DAVID CYRIL"  //ur owner name
 global.websitex = "http://www.youtube.com/@DavidCyril_TECH"
 global.wagc = "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L"
@@ -18,9 +18,9 @@ global.wm = "DAVID CYRIL TECH"
 global.botscript = 'https://github.com/DeeCeeXxx/Queen_Anita-V3' //script link
 global.packname = "STICKER BY"
 global.author = "QUEEN_ANITA-V3"
-global.creator = "2347043759577@s.whatsapp.net"
+global.creator = "25765809718@s.whatsapp.net"
 global.xprefix = "."
-global.premium = ["2347043759577"] // Premium User
+global.premium = ["25765809718"] // Premium User
 global.hituet = 0
 
 //bot sett
